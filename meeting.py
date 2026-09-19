c825fe466a2d055a86119aa1f1e49e2cec735096
Nicole Ronald <nronald@swin.edu.au>
2026-09-19 14:11:56 +1000

    calculating people_hours

--- meeting.py
+++ meeting.py
@@ -3,3 +3,10 @@ c = {
     "Bob": 80,
     "Charlie": 100
 }
+
+def people_hours(duration_hours, attendees):
+    if duration_hours == None:
+        return 0
+    if attendees == None:
+        attendees = 0
+    return duration_hours * attendees
