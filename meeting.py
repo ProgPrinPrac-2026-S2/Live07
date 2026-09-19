17c4c0d12f9a0072e7ca5d8b62ffb89ca12603f0
Nicole Ronald <nronald@swin.edu.au>
2026-09-19 14:13:04 +1000

    add meeting cost

--- meeting.py
+++ meeting.py
@@ -11,4 +11,10 @@ def people_hours(duration_hours, attendees):
         attendees = 0
     return duration_hours * attendees
 
+def meeting_cost(duration_hours, cost_per_person):
+    total = 0
+    for person, rate in cost_per_person.items():
+        total = total + rate
+    return total / duration_hours
+
 print(people_hours(1.5, 6))
